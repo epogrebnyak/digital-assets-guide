4c405612e8d18dbb8d15dc47e9376509df5eb662
Погребняк Евгений <e.pogrebnyak@gmail.com>
2026-07-27 17:06:07 +0300

    another try

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://epogrebnyak.github.io/',
-    base: '/digital-assets-guide/', 
+    site: 'https://epogrebnyak.github',
+    base: '/digital-assets-guide', 
 	integrations: [
 		starlight({
 			title: 'Digital Assets Guide',
