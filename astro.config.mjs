d9ca40032ba8a2f8465ad9508ecc195c9f3dd1c4
Погребняк Евгений <e.pogrebnyak@gmail.com>
2026-07-27 16:47:25 +0300

    added base and site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,28 +4,12 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://epogrebnyak.github.io/digital-assets-guide',
+    base: '/digital-assets-guide/', 
 	integrations: [
 		starlight({
 			title: 'Digital Assets Guide',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://epogrebnyak.github.io/blockchain-theme-map/' }],
-			// sidebar: [
-				// {
-				// label: 'Essential!!!',
-				// autogenerate: { directory: 'essential' },
-				// // order: 2, // 👈 This determines the folder's position in the sidebar
-				// },
-			// 	{
-			// 		label: 'Guides',
-			// 		items: [
-			// 			// Each item here is one entry in the navigation menu.
-			// 			{ label: 'Example Guide', slug: 'guides/example' },
-			// 		],
-			// 	},
-			// 	{
-			// 		label: 'Reference',
-			// 		items: [{ autogenerate: { directory: 'reference' } }],
-			// 	},
-			// ],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/epogrebnyak/digital-assets-guide' }],
 		}),
 	],
 });
