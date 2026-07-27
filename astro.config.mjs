2be216a5c8d8e5fef53098c9017392f70ae8bbbf
Погребняк Евгений <e.pogrebnyak@gmail.com>
2026-07-27 17:36:42 +0300

    nojekyll file added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://epogrebnyak.github',
-    base: '/digital-assets-guide', 
+    site: 'https://epogrebnyak.github.io',
+    base: '/digital-assets-guide/',
+    build: {
+        assets: 'assets' // Change the default '_astro' folder to 'assets'
+    },
 	integrations: [
 		starlight({
 			title: 'Digital Assets Guide',
